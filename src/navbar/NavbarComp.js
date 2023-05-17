ef5b161d7fcc738b9995a8e7042a5427d135548f
Masum Ahmed Choudhuri <42108289+Minor-lazer@users.noreply.github.com>
2023-05-18 01:21:29 +0530

    Navbar Responsive

--- src/navbar/NavbarComp.js
+++ src/navbar/NavbarComp.js
@@ -10,20 +10,22 @@ import {FaWhatsapp } from 'react-icons/fa';
 const NavbarComp = () => {
   return (
     <>
-      <Navbar  className="custom-navbar">
-        <Container>
-          <Navbar.Brand href="#home" className="custom-navbar-brand" >
-            <Image className="rounded-circle" src={require('./MiniLogo.png')} width="60px" height="60px"/>
-            <p className="centered-text" >MiniCoders</p>
-          </Navbar.Brand>
-          <Nav className="me-auto custom-nav">
-            <Nav.Link href="#home" style={{color:"black" , fontSize:"18px"}}>Home</Nav.Link>
-            <Nav.Link href="#features" style={{color:"black", fontSize:"18px"}}>Features</Nav.Link>
-            <NavDropdown title="Course Details"  id="basic-nav-dropdown">
+     <Navbar bg="light" expand="lg" className="me-auto">
+     <Navbar.Brand href="#home" className="custom-navbar-brand">
+            <Image className="rounded-circle" src={require('./MiniLogo.png')} width="60px" height="60px" />
+            <p className="centered-text">MiniCoders</p>
+     </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto custom-nav">
+      <Nav.Link href="#home" style={{color:"black" , fontSize:"18px"}}>Home</Nav.Link>
+            <Nav.Link  href="#features" style={{color:"black", fontSize:"18px"}}>Features</Nav.Link>
+            <Nav.Link  href="#features" style={{color:"black", fontSize:"18px"}}>Features</Nav.Link>
+            <NavDropdown  title="Course Details"  id="basic-nav-dropdown">
             <NavDropdown.Item href="#">Python</NavDropdown.Item>
             <NavDropdown.Item href="#">Web development</NavDropdown.Item>
             <NavDropdown.Item href="#">Scratch</NavDropdown.Item>
-            <NavDropdown.Divider />
+            <NavDropdown.Divider/>
             <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="About Us" id="basic-nav-dropdown" style={{color:"white"}}>
@@ -34,15 +36,15 @@ const NavbarComp = () => {
             <NavDropdown.Item href="#">Careers</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
-          </NavDropdown>
-          </Nav>
-        </Container>
-        
-        <div class="custom-enquiry">
-        <FaWhatsapp style={{fontSize:"30px",marginRight:"5px"}}/>
-        <p class="enquiry-text">Enquiry</p>
-        </div>
-      </Navbar>
+            </NavDropdown>
+            <div class="custom-enquiry me-auto"> 
+            <FaWhatsapp style={{fontSize:"30px",marginRight:"5px"}}/>
+            <p class="enquiry-text">Enquiry</p>
+         </div>          
+      </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    
     </>
   );
 }
